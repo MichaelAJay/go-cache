ff6d6ca920b314313a21a11ebf588182ed518d32
mjay <michael.a.jay@protonmail.com>
2025-08-29 09:39:13 -0400

    moved files

--- internal/redis/factory.go
+++
@@ -1,5 +0,0 @@
-package redis
-
-// This file has been cleared as part of Phase 2 Redis-only refactoring
-// Factory functions and provider registration are eliminated
-// Use direct NewRedisCache[T] constructor instead
